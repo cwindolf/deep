6bfad1fa99bd59b3c530c7c94258e46b2cb55b1b
Charlie Windolf <cwindolf95@gmail.com>
2016-11-04 14:26:50 -0400

    hi eital

---
+++ gsv_api_fn.py
@@ -0,0 +1,12 @@
+
+def random_providence_image():
+    pass
+
+'''
+Refs:
+Random points:
+    using lat/long in a circle:
+    http://gis.stackexchange.com/questions/25877/how-to-generate-random-locations-nearby-my-location
+    in arbitrary polygons:
+    http://gis.stackexchange.com/questions/6412/generate-points-that-lie-inside-polygon
+'''
