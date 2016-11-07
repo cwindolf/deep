cd493c952ddd07d52bec1f908c2144abb14b246c
Charlie Windolf <cwindolf95@gmail.com>
2016-11-07 15:10:33 -0500

    stencils

--- char-rnn/char_rnn.py
+++ char-rnn/char_rnn.py
@@ -3,4 +3,28 @@ import tensorflow as tf
 import numpy as np
 
 
+class CharLSTM():
+
+    def __init__(embed_size, lstm_size, vocab_size, batch_size):
+        # Set up embeddings
+
+        # Set up 2-layer LSTM
+
+        # Use seq2seq.rnn_decoder instead of dynamic_rnn to run the cell
+
+        # softmax for logits
+
+        # seq2seq loss by example
+
+        # define trainer
+
+        pass
+
+
+    def train(sess, batches):
+        pass
+
+
+    def sample(sess, n, seed, vocab_index, chars):
+        pass
 
